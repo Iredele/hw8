aa522d2433a8b134b85c5f54ff9c42bf59215837
TheOneAboveAll <iredeledayo@yahoo.com>
2023-11-14 01:40:48 -0800

    first commit

---
+++ software/pt1/obj/default/hello_world.d
@@ -0,0 +1,41 @@
+obj/default/hello_world.o: hello_world.c ../pt1_bsp//HAL/inc/io.h \
+ ../pt1_bsp//HAL/inc/alt_types.h ../pt1_bsp/system.h ../pt1_bsp/linker.h \
+ ../pt1_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../pt1_bsp//drivers/inc/altera_avalon_timer.h \
+ ../pt1_bsp//HAL/inc/alt_types.h ../pt1_bsp//HAL/inc/sys/alt_dev.h \
+ ../pt1_bsp//HAL/inc/sys/alt_llist.h \
+ ../pt1_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../pt1_bsp//HAL/inc/sys/alt_warning.h \
+ ../pt1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../pt1_bsp//HAL/inc/sys/alt_irq.h ../pt1_bsp//HAL/inc/nios2.h \
+ ../pt1_bsp//HAL/inc/sys/alt_stdio.h
+
+../pt1_bsp//HAL/inc/io.h:
+
+../pt1_bsp//HAL/inc/alt_types.h:
+
+../pt1_bsp/system.h:
+
+../pt1_bsp/linker.h:
+
+../pt1_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../pt1_bsp//drivers/inc/altera_avalon_timer.h:
+
+../pt1_bsp//HAL/inc/alt_types.h:
+
+../pt1_bsp//HAL/inc/sys/alt_dev.h:
+
+../pt1_bsp//HAL/inc/sys/alt_llist.h:
+
+../pt1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../pt1_bsp//HAL/inc/sys/alt_warning.h:
+
+../pt1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../pt1_bsp//HAL/inc/sys/alt_irq.h:
+
+../pt1_bsp//HAL/inc/nios2.h:
+
+../pt1_bsp//HAL/inc/sys/alt_stdio.h:
---
+++ software/pt1_1/obj/default/hello_world.d
@@ -0,0 +1,38 @@
+obj/default/hello_world.o: hello_world.c ../gen/HAL/inc/io.h \
+ ../gen/HAL/inc/alt_types.h ../gen/system.h ../gen/linker.h \
+ ../gen/drivers/inc/altera_avalon_timer_regs.h \
+ ../gen/drivers/inc/altera_avalon_timer.h ../gen/HAL/inc/alt_types.h \
+ ../gen/HAL/inc/sys/alt_dev.h ../gen/HAL/inc/sys/alt_llist.h \
+ ../gen/HAL/inc/priv/alt_dev_llist.h ../gen/HAL/inc/sys/alt_warning.h \
+ ../gen/drivers/inc/altera_avalon_pio_regs.h ../gen/HAL/inc/sys/alt_irq.h \
+ ../gen/HAL/inc/nios2.h ../gen/HAL/inc/sys/alt_stdio.h
+
+../gen/HAL/inc/io.h:
+
+../gen/HAL/inc/alt_types.h:
+
+../gen/system.h:
+
+../gen/linker.h:
+
+../gen/drivers/inc/altera_avalon_timer_regs.h:
+
+../gen/drivers/inc/altera_avalon_timer.h:
+
+../gen/HAL/inc/alt_types.h:
+
+../gen/HAL/inc/sys/alt_dev.h:
+
+../gen/HAL/inc/sys/alt_llist.h:
+
+../gen/HAL/inc/priv/alt_dev_llist.h:
+
+../gen/HAL/inc/sys/alt_warning.h:
+
+../gen/drivers/inc/altera_avalon_pio_regs.h:
+
+../gen/HAL/inc/sys/alt_irq.h:
+
+../gen/HAL/inc/nios2.h:
+
+../gen/HAL/inc/sys/alt_stdio.h:
